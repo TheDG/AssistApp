bfeea70791be587abefbc47b4e4ef846ae4b63b8
Unknown <jhidalgo1@uc.cl>
2017-05-13 20:06:36 -0300

    Fix css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,6 @@
 //= require_tree .
 // Custom bootstrap variables must be set or imported *before* bootstrap.
 
-/*!
- * Bootstrap v3.3.7 (http://getbootstrap.com)
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under the MIT license
- */
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
