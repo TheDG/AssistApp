7d5d6ac41ff0f08986ed2699d0d57381f3bb692c
TheDG <dsinay@uc.cl>
2017-05-15 02:04:26 -0400

    Fix Auth for user with RUT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :rut])
+  end
+
 end
