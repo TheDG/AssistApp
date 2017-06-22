36fc51c9b339e8ae739934877fd14da52c372282
TheDG <dsinay@uc.cl>
2017-06-22 02:51:18 -0400

    Some Basic styling + layout chagnes

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,15 @@
 module CoursesHelper
+
+  def last_attendence
+    course_aux = nil
+    last_assist = Date.new(2010,1,1)
+    current_teacher.courses.each do |course|
+      course.assistances.each do |assist|
+        course_aux = course if assist.date > last_assist
+      end
+    end
+    course_aux
+  end
 end
+
+
--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,10 @@
 module TeachersHelper
+
+  def student_number
+    aux = 0
+    current_teacher.courses.each do |course|
+      aux += course.students.count
+    end
+    aux
+  end
 end
