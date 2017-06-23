8d2b037e814ea1a9fc1e46bfc02fea1815f49b32
TheDG <dsinay@uc.cl>
2017-06-23 01:56:47 -0400

    debugging3

--- app/controllers/api/content_controller.rb
+++ app/controllers/api/content_controller.rb
@@ -14,7 +14,8 @@ module Api
       # puts current_teacher
       # puts Student.where(rut: params[:rut]).first
       # puts Time.parse(params[:date])
-      time = Time.new(params[:date].year, params[:date].mon, params[:date].mday, 5)
+      send_time = Time.parse(params[:date])
+      time = Time.new(params[send_time.year, send_time.mon, send_time.mday, 5)
       aux = Assistance.where(student_id: Student.where(rut: params[:rut]).first.id,
                              date: time, attend: true).first
       unless aux
