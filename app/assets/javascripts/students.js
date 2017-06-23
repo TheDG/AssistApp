dc7e9be874fc8fae8028c9a39522d23cb687f1c8
TheDG <dsinay@uc.cl>
2017-06-22 21:50:21 -0400

    Fix Show COurse assistance table + change assit on click

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,13 +1,27 @@
-/*
+
 var func =(function() {
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "/students/2/generate_qr",
-        success: function(data){}
+    $('.change_assit').click(function(){
+        $.ajax({
+            type: "PUT",
+            dataType: "json",
+            url: "/assistances/"+$(this).data('assist_id')+"/change_assist",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+            },
+            success: function(data){
+                if (data.attend){
+                    $('#'+data.id).css("background-color", "#88ff88");
+                    $('#'+data.id).text('yes');
+
+                }
+                else{
+                    $('#'+data.id).css("background-color", "#ff8888");
+                    $('#'+data.id).text('no');
+                }
+            }
+        });
     });
+
 });
 
 $(document).on('turbolinks:load', func);
-
-*/
